e35b6e915ea826c4abe31ae4e78b9b9fa807f516
Dasan Chamika <dasanchamika@gmail.com>
2025-11-08 18:40:06 +0530

    Session 06 - CineScope Assessment

---
+++ components/main-nav.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { Logo } from "./logo";
+import { ModeToggle } from "./mode-toggle";
+
+export default function MainNav() {
+  return (
+    <header className="border-b border-primary/20 bg-background sticky top-0 z-50 w-full">
+      <div className="container mx-auto max-w-[87.5rem] px-8 flex h-16 items-center ">
+        <Link href="/" className=" flex items-center gap-1">
+          <Logo />
+          <span className="text-primary text-xl font-bold">CineScope</span>
+        </Link>
+
+        <nav className="ml-auto flex items-center gap-4">
+          <Link href="/movies">Movies</Link>
+          <Link href="/genres">Genres</Link>
+          <Link href="/about">About</Link>
+          <Link href="/admin">Admin</Link>
+          <ModeToggle />
+        </nav>
+      </div>
+    </header>
+  );
+}
