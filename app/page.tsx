8a6beb97eba5c69345821f854608cc2ba11720c8
Dasan Chamika <dasanchamika@gmail.com>
2025-11-09 20:19:14 +0530

    Day - 10 session (Developed the landing page design)

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,16 @@
 // RSC - React Server Component
 // SSR - Enabled by default import MainNav from "@/components/main-nav";
 
+import FeaturedMovies from "@/components/landing/featured-movies";
+import HeroBanner from "@/components/landing/hero-banner";
 import { MainNav } from "@/components/main-nav";
-import Image from "next/image";
-import Link from "next/link";
 export default function HomePage() {
   return (
-    <div className=" min-h-screen flex flex-col">
+    <div className=" min-h-screen flex flex-col relative">
       <MainNav />
-      <main className=" flex-12 text-4xl py-16 flex flex-col justify-center items-center gap-4">
-        This is Home Page
-        <Link
-          href="/movies"
-          className="text-white bg-black p-4 rounded-full text-sm"
-        >
-          Go to Movies Page
-        </Link>
-        <div>
-          <Image
-            src="/assets/naming-conventions.png"
-            alt="Naming Conventions"
-            width={800}
-            height={500}
-          />
-        </div>
+      <main className="">
+        <HeroBanner />
+        <FeaturedMovies />
       </main>
     </div>
   );
