4ad0918ae31ffb1a83feeb4c76b0d31b55889dc6
Dasan Chamika <dasanchamika@gmail.com>
2025-11-27 23:13:55 +0530

    Start working on register form

---
+++ components/register-form.tsx
@@ -0,0 +1,71 @@
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+export function RegisterForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className=" text-lg capitalize">
+            Create your account
+          </CardTitle>
+          <CardDescription>
+            Enter your details below to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup className=" gap-4">
+              <Field>
+                <FieldLabel htmlFor="name">Name</FieldLabel>
+                <Input id="name" type="text" placeholder="John Doe" required />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="john.doe@email.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit" className=" cursor-pointer">
+                  Register
+                </Button>
+                <Button variant="outline" type="button" disabled>
+                  Continue with Google
+                </Button>
+                <FieldDescription className="text-center cursor-pointer">
+                  Already have an account? <Link href="/login">Login</Link>
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
